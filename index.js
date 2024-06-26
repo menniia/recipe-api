19c9ceb68a386c3d01224243d0cfb33ef5d8664a
menniia <mennia17@gmail.com>
2024-06-26 16:18:59 +0000

    express middleware

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
-import recipeRouter from "./routes/recipes.js";
+import recipeRouter from "./routes/recipe.js";
+import mongoose from "mongoose";
+
+// connect to database
+await mongoose.connect(process.env.MONGO_URL);
 
 // create express app
 const app = express();
+
+// apply middlewares
+app.use(express.json());
 
 // define routes
 app.get("/", (req, res) => {
